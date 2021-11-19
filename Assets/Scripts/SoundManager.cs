2e3db55203cc09a92e22b3ef830ad0046006fa0d
UdithPremarathne <IT18137224@my.sliit.lk>
2021-11-19 21:50:13 +0530

    finalized commits

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static AudioClip playerHitSound, jumpSound, coinPickUpsound;
+
+    static AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerHitSound = Resources.Load<AudioClip>("Hit");
+        jumpSound = Resources.Load<AudioClip>("Jump");
+        coinPickUpsound = Resources.Load<AudioClip>("Coin");
+
+
+
+        audioSrc = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip){
+             case "Hit":
+                audioSrc.PlayOneShot(playerHitSound);
+                break;
+
+            case "Jump":
+                audioSrc.PlayOneShot(jumpSound);
+                break;
+
+            case "Coin":
+                audioSrc.PlayOneShot(coinPickUpsound);
+                break;
+           
+        }
+    }
+}
---
+++ Assets/Scripts/TrapObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class TrapObject : MonoBehaviour
+{
+    private void Reset()
+    {
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            FindObjectOfType<LifeCount>().LoseLife();
+            Debug.Log("Assen giya");
+        }
+    }
+}
